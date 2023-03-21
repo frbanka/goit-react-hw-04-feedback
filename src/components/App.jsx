c364d18014974e9fe0481f41e6683400631b4047
Jacek Wójciak <fr.banka@gmail.com>
2023-03-21 20:35:01 +0100

    base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return <div>Please leave feedback</div>;
+  }
+}
