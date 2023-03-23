7c17600fece34ac230507a40b30a4cd27fb8c543
Jacek Wójciak <fr.banka@gmail.com>
2023-03-23 17:57:14 +0100

    render

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
